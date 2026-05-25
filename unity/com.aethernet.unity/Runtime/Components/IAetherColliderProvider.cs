9df15617a9f173c0e4fa690ec9527f802bbdf395
AdielMag <adiel12430@gmail.com>
2026-05-25 13:08:19 +0300

    chore: remove UPM workflow and unity package

--- unity/com.aethernet.unity/Runtime/Components/IAetherColliderProvider.cs
+++
@@ -1,9 +0,0 @@
-using nkast.Aether.Physics2D.Dynamics;
-
-namespace AetherNet
-{
-    public interface IAetherColliderProvider
-    {
-        void AttachToBody(Body body, PhysicsWorldManager world);
-    }
-}
